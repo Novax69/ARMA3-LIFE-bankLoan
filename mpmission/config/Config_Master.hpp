4939d7f7fd83227e2edbc0858b98b84dc0fea7e6
Novax69 <damien.petitjean@laposte.net>
2020-04-29 10:42:23 +0200

    NPC Code

--- mpmission/config/Config_Master.hpp
+++ mpmission/config/Config_Master.hpp
@@ -1,9 +1,13 @@
 class Life_Settings {
 	/* Add at the beginning */
 
+	//variable similar to all my script if you have it don't copy it
+	nov_useDonator = false; //Enable donators bonuses
+
+
 	//Params used for Loan
 	nov_loanAmmountLimit = 300000; // Can't ask for a loan bigger than 300.000 $
-	nov_interestPercent = 2;
+	nov_interestPercent = 10; // Percent interest for non donor
 	// Ends of Loan Params 
 
 };
